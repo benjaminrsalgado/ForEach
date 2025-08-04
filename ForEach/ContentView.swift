f5f848be0b97e8bbfff67d044b862da4d34ec001
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-04 14:20:52 -0600

    i practice foreach

--- ForEach/ContentView.swift
+++ ForEach/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let dias = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView{
+                ForEach(dias, id:\.self){ dia in
+                    Text(dia)
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.7))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                                  
+                }
+            }
+            .padding(30)
+         
         }
-        .padding()
+   
     }
 }
 
