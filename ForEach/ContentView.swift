956b7d58e19f0ffccffb34bb4e58bd6dbbf375fb
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-04 14:09:02 -0600

    Initial Commit

---
+++ ForEach/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForEach
+//
+//  Created by Benjamin Rojo on 04/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForEach/ForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachApp.swift
+//  ForEach
+//
+//  Created by Benjamin Rojo on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
